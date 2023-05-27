acdf2640163f71e71914e8f953688977c8ad1477
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-05-27 11:16:15 -0400

    Update with notes from learnings

--- App/Program.cs
+++ App/Program.cs
@@ -14,61 +14,80 @@ namespace LoggingOtelExporter
     {
         static void Main(string[] args)
         {
-            string tracingendpoint = Environment.GetEnvironmentVariable("FIRSTPARTY_TRACING_GRPC_ENDPOINT") ?? "localhost";
-            string loggingendpoint = Environment.GetEnvironmentVariable("FIRSTPARTY_LOGGING_GRPC_ENDPOINT") ?? "localhost";
+            string tracingendpoint =
+                Environment.GetEnvironmentVariable("FIRSTPARTY_TRACING_GRPC_ENDPOINT")
+                ?? "localhost";
+            string loggingendpoint =
+                Environment.GetEnvironmentVariable("FIRSTPARTY_LOGGING_GRPC_ENDPOINT")
+                ?? "localhost";
 
-            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+                true
+            );
 
             using var loggerFactory = LoggerFactory.Create(builder =>
             {
-                builder.Services.AddOpenTelemetryTracing(
-                        b =>
-                        {
-                            b.SetSampler(new AlwaysOnSampler())
-                            .AddSource("Microsoft.AzureArcData.GenevaDemo.LoggingOtelExporter");
-                        }
-                    );
+                builder.Services.AddOpenTelemetryTracing(b =>
+                {
+                    b.SetSampler(new AlwaysOnSampler())
+                        .AddSource("Microsoft.AzureArcData.GenevaDemo.LoggingOtelExporter");
+                });
 
                 builder.AddOpenTelemetry(options =>
                 {
                     options.AddConsoleExporter();
-                    options.AddOtlpExporter(
-                            otelOptions => {
-                                otelOptions.Endpoint = new Uri($"http://{loggingendpoint}");
-                                otelOptions.Protocol = OtlpExportProtocol.Grpc;
-                            }
-                        );
+                    options.AddOtlpExporter(otelOptions =>
+                    {
+                        otelOptions.Endpoint = new Uri($"http://{loggingendpoint}");
+                        otelOptions.Protocol = OtlpExportProtocol.Grpc;
+                    });
                 });
             });
 
             var serviceName = "Microsoft.AzureArcData.GenevaDemo";
             var serviceVersion = "1.0.0";
 
-
             using var tracerProvider = Sdk.CreateTracerProviderBuilder()
                 .AddSource(serviceName)
                 .SetResourceBuilder(
-                    ResourceBuilder.CreateDefault()
-                        .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+                    ResourceBuilder
+                        .CreateDefault()
+                        .AddService(serviceName: serviceName, serviceVersion: serviceVersion)
+                )
                 .AddConsoleExporter()
                 .AddOtlpExporter(config =>
-                    {
-                        config.Endpoint = new Uri($"http://{tracingendpoint}");
-                        config.Protocol = OtlpExportProtocol.Grpc;
-                    }
-                )
+                {
+                    config.Endpoint = new Uri($"http://{tracingendpoint}");
+                    config.Protocol = OtlpExportProtocol.Grpc;
+                })
                 .Build();
 
             var MyActivitySource = new ActivitySource(serviceName);
 
-            var logger = loggerFactory.CreateLogger("Log");
+            // This makes it in to dotnet.ilogger.category
+            //
+            // Useful links:
+            //
+            // - https://learn.microsoft.com/en-us/dotnet/core/extensions/logging?tabs=command-line#log-category
+            // - https://eng.ms/docs/products/geneva/collect/instrument/opentelemetrydotnet/otel-logs-deep-dive#example-3---structured-logging-with-dedicated-table
+            // - https://eng.ms/docs/products/geneva/collect/instrument/opentelemetrydotnet/configurationoptions#tablenamemappings-optional
+            //
+            // **NOTE**: ACA does NOT have table mapping support yet -
+            // everything goes into `Logs` - so for now, we filter on Category:
+            //
+            //   - https://msazure.visualstudio.com/One/_git/EngSys-MDA-GenevaDocs?path=/documentation/getting_started/environments/azurecontainerapps.md&version=GBmaster&_a=preview&anchor=known-limitation
+            //
+            var loggerName = "OtelGenevaDemo";
+            var logger = loggerFactory.CreateLogger(loggerName);
 
-            logger.LogInformation($"FIRSTPARTY_TRACING_GRPC_ENDPOINT: {tracingendpoint} FIRSTPARTY_LOGGING_GRPC_ENDPOINT {loggingendpoint}");
+            logger.LogInformation(
+                $"FIRSTPARTY_TRACING_GRPC_ENDPOINT: {tracingendpoint} FIRSTPARTY_LOGGING_GRPC_ENDPOINT {loggingendpoint}"
+            );
 
             int counter = 0;
             while (true)
             {
-
                 using var activity = MyActivitySource.StartActivity("SayHello");
                 {
                     activity?.SetTag("Tag1", 1);
@@ -76,6 +95,8 @@ namespace LoggingOtelExporter
 
                     activity?.SetStatus(ActivityStatusCode.Ok);
 
+                    // We do NOT include the formatted template, because we'll get wider columns for our Kusto table.
+                    //
                     logger.LogInformation($"[Logging endpoint: {loggingendpoint}] [Tracing endpoint: {tracingendpoint}] [OTEL: 1.3.0-rc.2] Counter: {++counter}");
                 }
 
